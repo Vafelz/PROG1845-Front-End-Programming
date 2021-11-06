f758bf20e170101afde128a6c61424374e4ba09d
Duke Do <50648115+Vafelz@users.noreply.github.com>
2021-11-06 15:07:18 -0400

    211106.1506: Completed assignment 1 part 2
    + Added 4th test score
    + Added display in dialog box

--- Assignment 1/test_scores/test_scores.js
+++ Assignment 1/test_scores/test_scores.js
@@ -7,14 +7,26 @@ const score2 = parseInt(prompt("Enter test score"));
 
 const score3 = parseInt(prompt("Enter test score"));
 
-const total = score1 + score2 + score3;
+const score4 = parseInt(prompt("Enter test score"));
+
+const total = score1 + score2 + score3 + score4;
 
 // calculate the average
-const average = parseInt(total/3);
+const average = parseInt(total/4);
 
 // display in browser page
 const html = `<p>Score 1 = ${score1}</p>
-    <p>Score 2 = ${score2}</p>
-    <p>Score 3 = ${score3}</p>
-    <p>Average score = ${average}</p>`;
+<p>Score 2 = ${score2}</p>
+<p>Score 3 = ${score3}</p>
+<p>Score 4 = ${score4}</p>
+<p>Average score = ${average}</p>`;
 document.write(html);
+
+// display in dialog box
+window.alert(
+`Score 1 = ${score1}
+Score 2 = ${score2}
+Score 3 = ${score3}
+Score 4 = ${score4}
+
+Average score = ${average}`)
