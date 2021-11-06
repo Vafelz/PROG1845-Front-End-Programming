f2d7165b8110f7b570ba1eecf2188dc2ea3d14d9
Duke Do <50648115+Vafelz@users.noreply.github.com>
2021-11-06 16:06:44 -0400

    211106.1605: Completed assignment 3 part 2: test score array
    + Added displayScore function
    + Added #display_scores click event to display scores in #scores_display
    + Added clear #scores_display when addScore function run

--- Assignment 3/test_scores/test_scores.js
+++ Assignment 3/test_scores/test_scores.js
@@ -1,41 +1,49 @@
 "use strict"
 
-const names = ["Ben", "Joel", "Judy", "Anne"];
-const scores = [88, 98, 77, 88];
+const names = ["Ben", "Joel", "Judy", "Anne"]
+const scores = [88, 98, 77, 88]
 
-const $ = selector => document.querySelector(selector);
+const $ = selector => document.querySelector(selector)
 
 const addScore = () => {
-	// get user entries
-	const name = $("#name").value;
-	const score  = parseInt( $("#score").value );
-	let isValid = true;
-    
+    // get user entries
+    const name = $("#name").value
+    const score = parseInt($("#score").value)
+    let isValid = true
+
     // check entries for validity
     if (name == "") {
-		$("#name").nextElementSibling.textContent = "This field is required.";
-		isValid = false;
-	} else {
-		$("#name").nextElementSibling.textContent = "";
+        $("#name").nextElementSibling.textContent = "This field is required."
+        isValid = false
+    } else {
+        $("#name").nextElementSibling.textContent = ""
+    }
+
+    if (isNaN(score) || score < 0 || score > 100) {
+        $("#score").nextElementSibling.textContent = "You must enter a valid score."
+        isValid = false
+    } else {
+        $("#score").nextElementSibling.textContent = ""
+    }
+
+    if (isValid) {
+        names[names.length] = name
+        scores[scores.length] = score
+        $("#name").value = ""
+        $("#score").value = ""
+    }
+    $("#scores_display").value = ""
+    $("#name").focus()
+}
+
+const displayScore = () => {
+	for (let i = 0; i < names.length; i++) {
+		$("#scores_display").value += names[i] + " = " + scores[i] + "\n"
 	}
-	
-	if (isNaN(score) || score < 0 || score > 100) {
-		$("#score").nextElementSibling.textContent = "You must enter a valid score.";
-		isValid = false;
-	} else {
-		$("#score").nextElementSibling.textContent = "";
-	}
-	
-	if (isValid) {
-		names[names.length] = name;
-		scores[scores.length] = score;
-	    $("#name").value = "";
-		$("#score").value = "";
-	}
-    $("#name").focus();
-};
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-	$("#add").addEventListener("click", addScore);
-	$("#name").focus();
-});
+    $("#add").addEventListener("click", addScore)
+    $("#display_scores").addEventListener("click", displayScore)
+    $("#name").focus()
+})
