8338157362bee8005e39996de86354b36795803b
Duke Do <50648115+Vafelz@users.noreply.github.com>
2021-11-13 19:21:10 -0500

    211113.1921: Completed assignment 5 part 1

--- Assignment 5/reservation/reservation.js
+++ Assignment 5/reservation/reservation.js
@@ -1,6 +1,23 @@
-"use strict";
+"use strict"
 
 $(document).ready( () => {
-	const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
-		
-}); // end ready
+	var emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/
+
+	// Focus on textbox with id 'arrival_date'
+	$('#arrival_date').focus()
+
+	// Submit event to validate user entries
+	$('#submit').click(() => {
+		// Check to validate textboxes are not empty
+		if ($.trim($('input[type=text]').val()) == "") {
+			alert("This field is required.")
+			return false
+		} else if (isNaN($('#nights').val().trim())) {
+			alert("Must be numeric.")
+			return false
+		} else if (emailPattern.test($('#email').val().trim()) == false) {
+			alert("Must be a valid email address.")
+			return false
+		}
+	})
+}) // end ready
